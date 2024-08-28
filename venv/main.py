87a25dd2c2b4fd88289fb38f2013d93e746370dd
Grace-0710 <badzoo@naver.com>
2024-08-28 11:04:50 +0900

    .

--- venv/main.py
+++ venv/main.py
@@ -2,26 +2,16 @@ from dotenv import load_dotenv
 from src import MilVus, DataMilVus, MilvusMeta
 from src import OpenAIQT, MistralQT, RulebookQR
 from src import EmbModel, LLMOpenAI, LLMMistral
+from src import EnvManager
 import os 
 import argparse
 import json
 
 
-def set_default_env(args):
-    pass
-
-def main(args):
-    load_dotenv()
-    db_name = 'finger'
-    ip_addr = os.getenv('ip_addr')
-    cohere_api = os.getenv('COHERE_API_KEY')
-
+def set_vectordb(args, ip_addr):
     with open(os.path.join(args.config_path, args.db_config)) as f:
         db_config = json.load(f)
     db_config['ip_addr'] = ip_addr
-
-    with open(os.path.join(args.config_path, args.llm_config)) as f:
-        llm_config = json.load(f)
 
     milvus_db = MilVus(db_config)
     milvus_db.set_env()
@@ -29,17 +19,31 @@ def main(args):
     meta_milvus = MilvusMeta()
     meta_milvus.set_rulebook_map()
     rulebook_eng_to_kor = meta_milvus.rulebook_eng_to_kor
-    # print(rulebook_eng_to_kor)
 
     collection = milvus_db.get_collection(args.collection_name)
     collection.load()
-    
+
+    milvus_db.get_partition(collection)
+    partition_list = [rulebook_eng_to_kor[p_name] for p_name in milvus_db.partition_names if not p_name.startswith('_')]
+    return data_milvus, partition_list, collection
+
+def set_llm(args):
+    global llm_config
+
+    with open(os.path.join(args.config_path, args.llm_config)) as f:
+        llm_config = json.load(f)
     emb_model = EmbModel()   # Embedding Model: bge-m3
     emb_model.set_embbeding_config()
-    
+    llm_rs = LLMOpenAI(llm_config)   # Response Generator
+    llm_rs.set_generation_config()
+    return emb_model, llm_rs 
+
+def set_query_translator(llm_config):
     llm_qt = OpenAIQT(llm_config)   # Query Translator
     llm_qt.set_generation_config()
+    return llm_qt
 
+def set_query_router(cohere_api):
     rulebook_qr = RulebookQR()   # Query Router
     rulebook_qr.create_prompt_injection_utterances()
     rulebook_qr.create_rulebook_utterances()
@@ -47,15 +51,21 @@ def main(args):
     rulebook_route = rulebook_qr.create_route('rulebook_check', rulebook_qr.prompt_rulebook_check_utterances)
     route_encoder = rulebook_qr.get_cohere_encoder(cohere_api)
     rulebook_rl = rulebook_qr.create_route_layer(route_encoder, [prompt_injection_route, rulebook_route])
+    return rulebook_qr, rulebook_rl
 
-    llm_rs = LLMOpenAI(llm_config)   # Response Generator
-    llm_rs.set_generation_config()
+def main(args):
+    load_dotenv() 
+    ip_addr = os.getenv('ip_addr')
+    cohere_api = os.getenv('COHERE_API_KEY')
 
+    data_milvus, partition_list, collection = set_vectordb(args, ip_addr)
+    emb_model, llm_rs = set_llm(args)
+    llm_qt = set_query_translator(llm_config)
+    rulebook_qr, rulebook_rl = set_query_router(cohere_api)
+    
     flag = True 
     threshold = 0.65
-    milvus_db.get_partition(collection)
-    partition_list = [rulebook_eng_to_kor[p_name] for p_name in milvus_db.partition_names if not p_name.startswith('_')]
-    # print(', '.join(partition_list))
+    
     print(f"대화를 시작해보세요 ! 회사 내부 규정에 대해 설명해주는 챗봇입니다.")
     print(f"* 현재 {', '.join(partition_list)}에 대한 질의응답이 가능합니다.")
     while(flag):
@@ -71,7 +81,7 @@ def main(args):
         print(f'routed query: {routed_query}', end='\n\n')
         if rulebook_rl(stepback_q).name == 'prompt_injection':    # 부적절한 발화가 입력된 경우   
             print(llm_rs.get_response(routed_query))
-        else: 
+        else:
             print(f'Step 3. 가상 문서를 생성합니다.')
             hyde = llm_qt.get_response(stepback_q)
             print(f'생성된 문서: {hyde}', end='\n\n')
@@ -82,7 +92,7 @@ def main(args):
             data_milvus.set_search_params(query_emb, output_fields='text')   
             search_params = data_milvus.search_params
             search_result = data_milvus.search_data(collection, search_params)
-            print(f'추출된 정보: {search_result}', end='\n\n')
+            # print(f'추출된 정보: {search_result}', end='\n\n')
             
             print(f'Step 5. 추출된 정보를 재조정합니다.')
             id_list = []; dist_list = []
@@ -113,7 +123,6 @@ if __name__ == '__main__':
     cli_parser.add_argument('--db_config', type=str, default='db_config.json')
     cli_parser.add_argument('--llm_config', type=str, default='llm_config.json')
     cli_parser.add_argument('--output_dir', type=str, default='../../data/pdf/embed_output')
-    cli_parser.add_argument('--file_name', type=str, default='취업규칙.csv')
     cli_parser.add_argument('--collection_name', type=str, default='rule_book')
     cli_argse = cli_parser.parse_args()
     main(cli_argse)
